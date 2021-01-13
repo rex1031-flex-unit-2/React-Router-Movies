e76dba4f68fceab6f6b09470bc5df5a45be3126c
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-01-12 19:25:24 -0500

    wrapped app in router, starting routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +15,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
+          console.log(response)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +33,8 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      {/* <Route path="/" component={MovieList} />
+      <Route /> */}
     </div>
   );
 }
